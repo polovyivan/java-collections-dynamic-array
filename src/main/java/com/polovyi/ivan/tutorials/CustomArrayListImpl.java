51d447f8421952b22d3b43f277116c99aa6f5736
Ivan Polovyi <ivan.polovyi@sensedia.com>
2022-11-13 15:19:09 -0300

    modified final version

--- src/main/java/com/polovyi/ivan/tutorials/CustomArrayListImpl.java
+++ src/main/java/com/polovyi/ivan/tutorials/CustomArrayListImpl.java
@@ -1,72 +1,36 @@
 package com.polovyi.ivan.tutorials;
 
-import java.util.List;
-
 public class CustomArrayListImpl<T> implements CustomArrayList<T> {
 
     private static final int DEFAULT_CAPACITY = 5;
 
     private int size;
     private int capacity;
-    private Object[] data;
+    private Object[] internalArray;
 
     public CustomArrayListImpl(int capacity) {
         this.capacity = capacity;
-        data = new Object[capacity];
+        internalArray = new Object[this.capacity];
     }
 
     public CustomArrayListImpl() {
         this.capacity = DEFAULT_CAPACITY;
-        data = new Object[capacity];
-    }
-
-    @Override
-    public String toString() {
-        String value = "[";
-
-        for (int i = 0; i < size; i++) {
-            value = value + data[i];
-            if (i != size - 1) {
-                value = value + ",";
-            }
-        }
-        value = value + "]";
-        return value;
-    }
-
-    @Override
-    public void add(T value) {
-        if (capacity == size) {
-            resize();
-        }
-        data[size] = value;
-        size++;
+        internalArray = new Object[this.capacity];
     }
 
     @Override
     public void set(int index, T value) {
-        this.data[index] = value;
-    }
-
-    @Override
-    public void insert(int index, T value) {
-        if (capacity == size) {
-            resize();
-        }
-        shiftUpFromIndex(index);
-        data[index] = value;
-        size++;
+        this.internalArray[index] = value;
     }
 
     @Override
     public T get(int index) {
-        return (T) this.data[index];
+        return (T) this.internalArray[index];
     }
 
     @Override
-    public void delete(int index) {
-        shiftDownToIndex(index);
-        size--;
+    public int size() {
+        return this.size;
     }
 
     @Override
@@ -75,10 +39,45 @@ public class CustomArrayListImpl<T> implements CustomArrayList<T> {
     }
 
     @Override
+    public void add(T value) {
+        if (capacity == size) {
+            resize();
+        }
+        internalArray[size] = value;
+        size++;
+    }
+
+    @Override
+    public void insert(int index, T value) {
+
+        if (capacity == size) {
+            resize();
+        }
+
+        if (size == index) {
+            add(value);
+            return;
+        }
+
+        shiftUpFromIndex(index);
+        internalArray[index] = value;
+        size++;
+    }
+
+    @Override
+    public void delete(int index) {
+        if (index != this.size) {
+            shiftDownToIndex(index);
+        }
+        this.internalArray[this.size] = null;
+        size--;
+    }
+
+    @Override
     public boolean contains(T value) {
-        for (int i = 0; i <= size; i++) {
-            T currentValue = (T) data[i];
-            if (currentValue.equals(value)) {
+        for (int i = 0; i <= this.size; i++) {
+            T currentValue = (T) internalArray[i];
+            if (currentValue != null && currentValue.equals(value)) {
                 return true;
             }
         }
@@ -86,30 +85,43 @@ public class CustomArrayListImpl<T> implements CustomArrayList<T> {
     }
 
     @Override
-    public void resize() {
-        int newCapacity = this.capacity * 2;
-        Object[] resizedData = new Object[newCapacity];
-        for (int i = 0; i < this.data.length; i++) {
-            resizedData[i] = this.data[i];
+    public String toString() {
+        String value = "[";
+
+        for (int i = 0; i < size; i++) {
+            value = value + internalArray[i];
+            if (i != size - 1) {
+                value = value + ", ";
+            }
         }
-        this.data = resizedData;
+        value = value + "]";
+        return value;
     }
 
-    @Override
-    public int size() {
-        return this.data.length;
+    public int getCapacity() {
+        return this.capacity;
     }
 
     private void shiftUpFromIndex(int index) {
-        for (int i = index; i > index; i--) {
-            data[i] = data[i - 1];
+        for (int i = this.size; i > index; i--) {
+            internalArray[i] = internalArray[i - 1];
         }
     }
 
     private void shiftDownToIndex(int index) {
-        for (int i = index; i > this.size - 1; i++) {
-            data[i] = data[i + 1];
+        for (int i = index; i < this.size; i++) {
+            internalArray[i] = internalArray[i + 1];
         }
-
     }
+
+    private void resize() {
+        int newCapacity = this.capacity * 2;
+        Object[] resizedData = new Object[newCapacity];
+        for (int i = 0; i < this.size; i++) {
+            resizedData[i] = this.internalArray[i];
+        }
+        this.internalArray = resizedData;
+        this.capacity = newCapacity;
+    }
+
 }
--- src/test/java/com/polovyi/ivan/tutorials/CustomArrayListTest.java
+++ src/test/java/com/polovyi/ivan/tutorials/CustomArrayListTest.java
@@ -2,31 +2,206 @@ package com.polovyi.ivan.tutorials;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CustomArrayListTest {
 
     @Test
-    public void getMethodTest() {
-
-        CustomArrayListImpl<Integer> integerCustomArrayList = new CustomArrayListImpl<>();
-        integerCustomArrayList.set(1, 2);
-
-        Integer integer = integerCustomArrayList.get(1);
-
-        System.out.println("integer = " + integer);
-
+    public void shouldCreateCustomArrayListWithDefaultCapacity() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        // when
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        customArrayList.add(4);
+        customArrayList.add(5);
+        // then
+        assertEquals(5, customArrayList.getCapacity());
     }
 
     @Test
-    public void insertMethodTest() {
+    public void shouldCreateCustomArrayListWithSpecifiedCapacity() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>(2);
+        // when
+        customArrayList.add(1);
+        customArrayList.add(2);
+        // then
+        assertEquals(2, customArrayList.getCapacity());
+    }
 
-        CustomArrayListImpl<Integer> integerCustomArrayList = new CustomArrayListImpl<>();
-        integerCustomArrayList.add(1);
-        integerCustomArrayList.add(2);
+    @Test
+    public void shouldBeEmpty() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        // when
+        boolean isEmpty = customArrayList.isEmpty();
+        // then
+        assertTrue(isEmpty);
+    }
 
-        Integer integer = integerCustomArrayList.get(0);
+    @Test
+    public void shouldDoubleCustomArrayListCapacity() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>(2);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        // when
+        customArrayList.add(3);
+        // then
+        assertEquals(4, customArrayList.getCapacity());
+    }
 
-        System.out.println("integer = " + integerCustomArrayList);
+    @Test
+    public void shouldSetTheValueAtSpecifiedIndex() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        // when
+        customArrayList.set(0, 3);
+        // then
+        assertTrue(3 == customArrayList.get(0));
+        assertEquals(2, customArrayList.size());
+        assertEquals(5, customArrayList.getCapacity());
+    }
 
+    @Test
+    public void shouldReturnCorrectSize() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>(2);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        // when
+        customArrayList.add(3);
+        // then
+        assertEquals(3, customArrayList.size());
+    }
+
+    @Test
+    public void shouldAddElementToCustomArray() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>(3);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        // when
+        customArrayList.add(3);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(2 == customArrayList.get(1));
+        assertTrue(3 == customArrayList.get(2));
+        assertEquals(3, customArrayList.size());
+    }
+
+    @Test
+    public void shouldInsertElementAtSpecifiedIndex() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        customArrayList.insert(1, 4);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(4 == customArrayList.get(1));
+        assertTrue(2 == customArrayList.get(2));
+        assertTrue(3 == customArrayList.get(3));
+        assertEquals(4, customArrayList.size());
+    }
+
+    @Test
+    public void shouldInsertAsLastWhenIndexEqualsToSize() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        customArrayList.insert(3, 4);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(2 == customArrayList.get(1));
+        assertTrue(3 == customArrayList.get(2));
+        assertTrue(4 == customArrayList.get(3));
+        assertEquals(4, customArrayList.size());
+    }
+
+    @Test
+    public void shouldResizeAndInsertAsLastWhenIndexEqualsToSize() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>(3);
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        customArrayList.insert(3, 4);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(2 == customArrayList.get(1));
+        assertTrue(3 == customArrayList.get(2));
+        assertTrue(4 == customArrayList.get(3));
+        assertEquals(4, customArrayList.size());
+        assertEquals(6, customArrayList.getCapacity());
+    }
+
+    @Test
+    public void shouldDeleteElementAtSpecifiedIndex() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        customArrayList.delete(1);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(3 == customArrayList.get(1));
+        assertEquals(2, customArrayList.size());
+    }
+
+    @Test
+    public void shouldDeleteAsLastElementWhenIndexEqualsToSize() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        customArrayList.delete(2);
+        // then
+        assertTrue(1 == customArrayList.get(0));
+        assertTrue(2 == customArrayList.get(1));
+        assertEquals(2, customArrayList.size());
+    }
+
+    @Test
+    public void shouldReturnTrueWhenCustomArrayListContainsValue() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        boolean contains = customArrayList.contains(2);
+        // then
+        assertTrue(contains);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCustomArrayListDoesNotContainValue() {
+        // given
+        CustomArrayListImpl<Integer> customArrayList = new CustomArrayListImpl<>();
+        customArrayList.add(1);
+        customArrayList.add(2);
+        customArrayList.add(3);
+        // when
+        boolean contains = customArrayList.contains(4);
+        // then
+        assertFalse(contains);
     }
 
 }
